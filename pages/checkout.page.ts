a3940f2031e1ed04cfa75f39ee7e727624dc10e6
Liviu Belibou <276375919+LiviuBelibou@users.noreply.github.com>
2026-09-11 13:50:30 +0200

    test: add checkout workflow coverage

---
+++ pages/checkout.page.ts
@@ -0,0 +1,53 @@
+import { type Locator, type Page } from '@playwright/test';
+
+export interface CustomerInformation {
+  firstName: string;
+  lastName: string;
+  postalCode: string;
+}
+
+export class CheckoutPage {
+  readonly page: Page;
+  readonly title: Locator;
+  readonly firstNameInput: Locator;
+  readonly lastNameInput: Locator;
+  readonly postalCodeInput: Locator;
+  readonly continueButton: Locator;
+  readonly errorMessage: Locator;
+  readonly subtotalLabel: Locator;
+  readonly taxLabel: Locator;
+  readonly totalLabel: Locator;
+  readonly finishButton: Locator;
+  readonly completeHeader: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.title = page.getByTestId('title');
+    this.firstNameInput = page.getByTestId('firstName');
+    this.lastNameInput = page.getByTestId('lastName');
+    this.postalCodeInput = page.getByTestId('postalCode');
+    this.continueButton = page.getByTestId('continue');
+    this.errorMessage = page.getByTestId('error');
+    this.subtotalLabel = page.getByTestId('subtotal-label');
+    this.taxLabel = page.getByTestId('tax-label');
+    this.totalLabel = page.getByTestId('total-label');
+    this.finishButton = page.getByTestId('finish');
+    this.completeHeader = page.getByTestId('complete-header');
+  }
+
+  async enterCustomerInformation(
+    customer: CustomerInformation,
+  ): Promise<void> {
+    await this.firstNameInput.fill(customer.firstName);
+    await this.lastNameInput.fill(customer.lastName);
+    await this.postalCodeInput.fill(customer.postalCode);
+  }
+
+  async continueCheckout(): Promise<void> {
+    await this.continueButton.click();
+  }
+
+  async finishCheckout(): Promise<void> {
+    await this.finishButton.click();
+  }
+}
